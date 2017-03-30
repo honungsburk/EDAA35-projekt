3d91afd0ec4b222be2563fbd0d07d95820c0357e
ma2536st-s <max.gw@live.se>
2017-03-30 13:58:25 +0200

    Util

--- src/MazeTimer.scala
+++ src/MazeTimer.scala
@@ -1,6 +1,18 @@
+import java.util.Scanner
+
 /**
   * Created by ha8040we-s on 30/03/17.
   */
 class MazeTimer {
 
+}
+class MazeUtil {
+  val Maze : Vector[Vector[Boolean]];
+
+  def readMazeFromFile(filename : String) : Unit = {
+    val scanner = new Scanner(new File(filename))
+    val tmpArray  = Array.ofDim[Int](1000,1000)
+
+
+  }
 }
